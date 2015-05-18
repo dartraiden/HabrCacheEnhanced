4f076d5fa5518648a641567263297d1a419318e6
dartraiden <wowemuh@gmail.com>
2015-05-18 18:45:15 +0300

    Add meta.js

---
+++ HabrCache.meta.js
@@ -0,0 +1,12 @@
+﻿// ==UserScript==
+// @name            HabrCache
+// @namespace       dotneter
+// @version         20150518.1
+// @description     Редиректит "Доступ к публикации закрыт" на кэш гугла
+// @icon            https://raw.githubusercontent.com/dartraiden/HabrCache/master/icon.png
+// @include         http://habrahabr.ru/*
+// @include         http://geektimes.ru/*
+// @include         http://megamozg.ru/*
+// @updateURL       https://github.com/dartraiden/HabrCache/raw/master/HabrCache.meta.js
+// @downloadURL     https://github.com/dartraiden/HabrCache/raw/master/HabrCache.user.js
+// ==/UserScript==
