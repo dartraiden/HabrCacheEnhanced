8c554e3ff9566a23a34bab0347afd61b55ba324b
dartraiden <wowemuh@gmail.com>
2015-06-10 11:47:12 +0300

    Rename script

--- HabrCache.meta.js
+++ HabrCache.meta.js
@@ -1,12 +1,12 @@
 ﻿// ==UserScript==
-// @name            HabrCache
-// @namespace       dotneter
-// @version         20150518.1
-// @description     Редиректит "Доступ к публикации закрыт" на кэш гугла
-// @icon            https://raw.githubusercontent.com/dartraiden/HabrCache/master/icon.png
-// @include         http://habrahabr.ru/*
+// @name            HabrCacheEnchanced
+// @description     Редиректит удалённые или скрытые в черновики записи на кэш гугла
+// @namespace       habrcache
+// @version         20150610.1
 // @include         http://geektimes.ru/*
+// @include         http://habrahabr.ru/*
 // @include         http://megamozg.ru/*
-// @updateURL       https://github.com/dartraiden/HabrCache/raw/master/HabrCache.meta.js
-// @downloadURL     https://github.com/dartraiden/HabrCache/raw/master/HabrCache.user.js
+// @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCache.meta.js
+// @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCache.user.js
+// @supportURL      https://github.com/dartraiden/HabrCacheEnchanced/issues
 // ==/UserScript==
--- HabrCache.user.js
+++ HabrCache.user.js
@@ -1,14 +1,14 @@
 ﻿// ==UserScript==
-// @name            HabrCache
-// @namespace       dotneter
-// @version         20150518.1
-// @description     Редиректит "Доступ к публикации закрыт" на кэш гугла
-// @icon            https://raw.githubusercontent.com/dartraiden/HabrCache/master/icon.png
-// @include         http://habrahabr.ru/*
+// @name            HabrCacheEnchanced
+// @description     Редиректит удалённые или скрытые в черновики записи на кэш гугла
+// @namespace       habrcache
+// @version         20150610.1
 // @include         http://geektimes.ru/*
+// @include         http://habrahabr.ru/*
 // @include         http://megamozg.ru/*
-// @updateURL       https://github.com/dartraiden/HabrCache/raw/master/HabrCache.meta.js
-// @downloadURL     https://github.com/dartraiden/HabrCache/raw/master/HabrCache.user.js
+// @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCache.meta.js
+// @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCache.user.js
+// @supportURL      https://github.com/dartraiden/HabrCacheEnchanced/issues
 // ==/UserScript==
 
 var h1 = document.querySelector("h1");
