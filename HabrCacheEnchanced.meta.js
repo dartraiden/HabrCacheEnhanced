2acc1e996955aa04801f9322949b4274820920df
dartraiden <wowemuh@gmail.com>
2016-01-31 04:19:16 +0300

    Add HTTPS support

--- HabrCacheEnchanced.meta.js
+++ HabrCacheEnchanced.meta.js
@@ -2,10 +2,13 @@
 // @name            HabrCacheEnchanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи на кэш гугла
 // @namespace       habrcache
-// @version         20160131.1
+// @version         20160131.2
 // @include         http://geektimes.ru/*
+// @include         https://geektimes.ru/*
 // @include         http://habrahabr.ru/*
+// @include         https://habrahabr.ru/*
 // @include         http://megamozg.ru/*
+// @include         https://megamozg.ru/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.meta.js
 // @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.user.js
 // @supportURL      https://github.com/dartraiden/HabrCacheEnchanced/issues
