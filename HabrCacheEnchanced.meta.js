f49fe36d7ce6549d21a031446b350519934aad5d
dartraiden <wowemuh@gmail.com>
2016-05-16 22:17:14 +0300

    Remove Мегамозг and HTTP support

--- HabrCacheEnchanced.meta.js
+++ HabrCacheEnchanced.meta.js
@@ -2,13 +2,9 @@
 // @name            HabrCacheEnchanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи на кэш гугла
 // @namespace       habrcache
-// @version         20160131.2
-// @include         http://geektimes.ru/*
+// @version         20160516.1
 // @include         https://geektimes.ru/*
-// @include         http://habrahabr.ru/*
 // @include         https://habrahabr.ru/*
-// @include         http://megamozg.ru/*
-// @include         https://megamozg.ru/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.meta.js
 // @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.user.js
 // @supportURL      https://github.com/dartraiden/HabrCacheEnchanced/issues
