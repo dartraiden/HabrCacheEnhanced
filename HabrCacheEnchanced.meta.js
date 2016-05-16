542cbb0c9ace3eebc822c37f413aa7685db479c5
dartraiden <wowemuh@gmail.com>
2016-05-16 22:30:07 +0300

    Restore HTTP support

--- HabrCacheEnchanced.meta.js
+++ HabrCacheEnchanced.meta.js
@@ -2,9 +2,11 @@
 // @name            HabrCacheEnchanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи на кэш гугла
 // @namespace       habrcache
-// @version         20160516.1
+// @version         20160516.2
 // @include         https://geektimes.ru/*
+// @include         http://geektimes.ru/*
 // @include         https://habrahabr.ru/*
+// @include         http://habrahabr.ru/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.meta.js
 // @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.user.js
 // @supportURL      https://github.com/dartraiden/HabrCacheEnchanced/issues
