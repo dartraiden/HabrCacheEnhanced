fc7b9b4097b26181c239e0f0434dc8d3a1e75642
dartraiden <wowemuh@gmail.com>
2018-05-30 03:24:16 +0300

    Remove Geektimes support

--- HabrCacheEnchanced.meta.js
+++ HabrCacheEnchanced.meta.js
@@ -3,7 +3,6 @@
 // @description     Перенаправляет удалённые или скрытые в черновики записи на кэш гугла
 // @namespace       habrcache
 // @version         20180424.1
-// @include         https://geektimes.com/*
 // @include         https://habr.com/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.meta.js
 // @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.user.js
