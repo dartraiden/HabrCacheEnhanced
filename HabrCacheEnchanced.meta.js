4f42dcde5f8a87b33fa5cd263310bb5d8741bfbe
dartraiden <wowemuh@gmail.com>
2018-06-13 22:08:11 +0300

    Add additional description

--- HabrCacheEnchanced.meta.js
+++ HabrCacheEnchanced.meta.js
@@ -1,8 +1,8 @@
 ﻿// ==UserScript==
 // @name            HabrCacheEnchanced
-// @description     Перенаправляет удалённые или скрытые в черновики записи с habr.com на кэш Google
+// @description     Перенаправляет удалённые или скрытые в черновики записи с habr.com на СоХабр и кэш Google
 // @namespace       habrcache
-// @version         20180424.1
+// @version         20180613.1
 // @include         https://habr.com/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.meta.js
 // @downloadURL     https://github.com/dartraiden/HabrCacheEnchanced/raw/master/HabrCacheEnchanced.user.js
