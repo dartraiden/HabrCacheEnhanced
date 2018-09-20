d6fa602a97ce5bf6bc2e13f02efae4d0578b9949
dartraiden <wowemuh@gmail.com>
2018-09-20 14:57:47 +0300

    It was a bad idea

--- HabrCacheEnhanced.user.js
+++ HabrCacheEnhanced.user.js
@@ -16,7 +16,7 @@ if(h1.innerHTML == "Доступ к публикации закрыт" || h1.inn
 	var lnks = document.getElementsByTagName('A');
 	for (var i = 0; i < lnks.length; ++i) {
 		if (lnks[i].className == 'button')
-			lnks[i].outerHTML += '<p>&bull; <a href="https://sohabr.net/habr/post/' + id + '/">СоХабр (Хабрахабр, Geektimes, Мегамозг)</a></p><p>&bull; <a href="https://itnan.ru/post.php?c=1&p=' + id + '">ITnan (только Хабрахабр)</a></p><p>&bull; <a href="https://webcache.googleusercontent.com/search?q=cache:' + document.location.href + '">Google Cache</a></p>';
+			lnks[i].outerHTML += '<p>&bull; <a href="https://sohabr.net/habr/post/' + id + '/">СоХабр</a></p><p>&bull; <a href="https://itnan.ru/post.php?c=1&p=' + id + '">ITnan</a></p><p>&bull; <a href="https://webcache.googleusercontent.com/search?q=cache:' + document.location.href + '">Google Cache</a></p>';
 	}
 }
 else if(h1.innerHTML == "прекратила активность на хабре"){
