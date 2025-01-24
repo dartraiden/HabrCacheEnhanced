6300b3b7a538830f4f8f7b4e28f9cf1bca938f94
dartraiden <wowemuh@gmail.com>
2025-01-25 01:38:07 +0300

    Another attempt to wait until the page is fully loaded

--- HabrCacheEnhanced.user.js
+++ HabrCacheEnhanced.user.js
@@ -2,7 +2,7 @@
 // @name            HabrCacheEnhanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи с habr.com на зеркала Хабра. Redirects deleted or hidden posts from habr.com to Habr mirrors.
 // @namespace       habrcache
-// @version         20250125.2
+// @version         20250125.3
 // @author          dartraiden
 // @match           https://habr.com/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnhanced/raw/master/HabrCacheEnhanced.meta.js
@@ -10,7 +10,7 @@
 // @supportURL      https://github.com/dartraiden/HabrCacheEnhanced/issues
 // ==/UserScript==
 
-window.addEventListener('load', function() {
+setTimeout(function() {
     var error_code = document.querySelector(".tm-error-message__code");
     if(error_code && error_code.innerText == "403") {
         var id = document.location.href.replace(/^.*\/(\d+)\/?.+/, '$1');
@@ -19,4 +19,4 @@ window.addEventListener('load', function() {
             body.outerHTML += '<p>&bull; <a href="https://itnan.ru/post.php?c=1&p=' + id + '">ITnan</a></p><p>&bull; <a href="https://sohabr.net/habr/post/' + id + '/">СоХабр</a></p><p>&bull; <a href="https://savepearlharbor.com/?p=' + id + '">SavePearlHarbor</a></p>';
         }
     }
-});
+}, 500);
