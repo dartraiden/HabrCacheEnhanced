d817c8ea36c021e8aee10b5d541214f6f07c6d73
dartraiden <wowemuh@gmail.com>
2025-01-25 01:01:25 +0300

    Add support for new 403 page design

--- HabrCacheEnhanced.meta.js
+++ HabrCacheEnhanced.meta.js
@@ -2,7 +2,7 @@
 // @name            HabrCacheEnhanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи с habr.com на зеркала Хабра. Redirects deleted or hidden posts from habr.com to Habr mirrors.
 // @namespace       habrcache
-// @version         20241207.2
+// @version         20250125.1
 // @author          dartraiden
 // @match           https://habr.com/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnhanced/raw/master/HabrCacheEnhanced.meta.js
--- HabrCacheEnhanced.user.js
+++ HabrCacheEnhanced.user.js
@@ -2,7 +2,7 @@
 // @name            HabrCacheEnhanced
 // @description     Перенаправляет удалённые или скрытые в черновики записи с habr.com на зеркала Хабра. Redirects deleted or hidden posts from habr.com to Habr mirrors.
 // @namespace       habrcache
-// @version         20241207.2
+// @version         20250125.1
 // @author          dartraiden
 // @match           https://habr.com/*
 // @updateURL       https://github.com/dartraiden/HabrCacheEnhanced/raw/master/HabrCacheEnhanced.meta.js
@@ -11,9 +11,9 @@
 // ==/UserScript==
 
 window.addEventListener('load', function() {
-    var h1 = document.querySelector("h1");
-    if(h1.innerText == "Доступ закрыт" || h1.innerText == "Access denied" || h1.innerText == "Доступ к публикации закрыт" || h1.innerText == "Access to this post is restricted" || h1.innerText == "Страница не найдена" || h1.innerText == "Page not found") {
+    var error_code = document.querySelector(".tm-error-message__code");
+    if(error_code.innerText == "403") {
         var id = document.location.href.replace(/^.*\/(\d+)\/?.+/, '$1');
-        h1.outerHTML += '<p>&bull; <a href="https://itnan.ru/post.php?c=1&p=' + id + '">ITnan</a></p><p>&bull; <a href="https://sohabr.net/habr/post/' + id + '/">СоХабр</a></p><p>&bull; <a href="https://savepearlharbor.com/?p=' + id + '">SavePearlHarbor</a></p>';
+        document.querySelector(".tm-error-message__body").outerHTML += '<p>&bull; <a href="https://itnan.ru/post.php?c=1&p=' + id + '">ITnan</a></p><p>&bull; <a href="https://sohabr.net/habr/post/' + id + '/">СоХабр</a></p><p>&bull; <a href="https://savepearlharbor.com/?p=' + id + '">SavePearlHarbor</a></p>';
     }
 });
